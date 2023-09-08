cb7c745e137b1ad67206fa159de4f1face1d927c
Nelzkie211 <neilandreireyes211@gmail.com>
2023-09-09 03:02:32 +0800

    }

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: process.env.NODE_ENV === "production" ? "/eggsy/" : "/",
-  base: '/eggsy/', // Use your repository name as the base path
+  base: "/eggsy/", // Use your repository name as the base path
   build: {
     rollupOptions: {
       input: {
-        main: process.env.NODE_ENV === 'production' ? '/eggsy/index.html' : '/index.html',
+        main:
+          process.env.NODE_ENV === "production"
+            ? "/eggsy/index.html"
+            : "/index.html",
         // Add the route for the custom 404.html page
-        '404': '/404.html',
+        404: "/404.html",
       },
     },
+  },
 });
