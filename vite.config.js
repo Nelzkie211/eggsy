7d381ec0105b2ac4bced58c6063a512810ee4daf
Nelzkie211 <neilandreireyes211@gmail.com>
2023-09-09 03:08:28 +0800

    com

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   plugins: [react()],
   // base: process.env.NODE_ENV === "production" ? "/eggsy/" : "/",
   base: "/eggsy/", // Use your repository name as the base path
-  build: {
-    rollupOptions: {
-      input: {
-        main: process.env.NODE_ENV === "production" ? NoPage : "/index.html",
-        // Add the route for the custom 404.html page
-        404: "/404.html",
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: process.env.NODE_ENV === "production" ? NoPage : "/index.html",
+  //       // Add the route for the custom 404.html page
+  //       404: "/404.html",
+  //     },
+  //   },
+  // },
 });
