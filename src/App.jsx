a00445cb5ba92b204f174f1f849c931dcbcc9533
Nelzkie211 <neilandreireyes211@gmail.com>
2023-09-08 23:49:22 +0800

    add temp routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@
 // import reactLogo from "./assets/react.svg";
 // import eggsyLogo from "./assets/img/eggsy.png";
 import Navbar from "./components/navbar/Navbar";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
       {/* <p>
         <i>@Nelzkie</i>
       </p> */}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,66 +1,96 @@
 // eslint-disable-next-line no-unused-vars
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import { Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+  Container,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import eggsy from "../../assets/img/eggsy.png";
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1, bgcolor: "rgb(0, 0, 0)" }}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ bgcolor: "rgb(0, 0, 0)" }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="home"
-            sx={{ mr: 2 }}
+        <Container>
+          <Toolbar
+            sx={{
+              "@media (max-width: 450px)": {
+                paddingX: "0",
+              },
+            }}
           >
-            <MenuIcon
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="home"
+              sx={{ mr: 1, display: { xs: "block", sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              display="flex"
+              alignItems="center"
+              component="div"
+              variant="h6"
               sx={{
-                display: { xs: "block", sm: "none" },
+                height: "48px",
+                color: "#fff",
+                textDecoration: "none",
+                position: "relative",
+                flexGrow: 1,
               }}
-            />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            EGGSY
-          </Typography>
-          <Button
-            color="inherit"
-            sx={{
-              textTransform: "capitalize",
-              display: { xs: "none", sm: "block" },
-            }}
-          >
-            Home
-          </Button>
-          <Button
-            color="inherit"
-            sx={{
-              textTransform: "capitalize",
-              display: { xs: "none", sm: "block" },
-            }}
-          >
-            About
-          </Button>
-          <Button
-            color="inherit"
-            sx={{
-              textTransform: "capitalize",
-              display: { xs: "none", sm: "block" },
-            }}
-          >
-            Branches
-          </Button>
-          <Button
-            color="inherit"
-            sx={{
-              textTransform: "capitalize",
-              display: { xs: "none", sm: "block" },
-            }}
-          >
-            Contact Us
-          </Button>
-        </Toolbar>
+            >
+              <a href="/">
+                <img
+                  src={eggsy}
+                  alt="EGGSY PUTO OVERLOAD"
+                  style={{ width: "100px", verticalAlign: "middle" }}
+                />
+              </a>
+            </Typography>
+            <Button
+              color="inherit"
+              sx={{
+                textTransform: "capitalize",
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              Home
+            </Button>
+            <Button
+              color="inherit"
+              sx={{
+                textTransform: "capitalize",
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              About
+            </Button>
+            <Button
+              color="inherit"
+              sx={{
+                textTransform: "capitalize",
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              Branches
+            </Button>
+            <Button
+              color="inherit"
+              sx={{
+                textTransform: "capitalize",
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              Contact Us
+            </Button>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
