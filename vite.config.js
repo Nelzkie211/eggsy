f0690798e111a8ec0b0e357ccc54c3f9cb0c706d
Nelzkie211 <neilandreireyes211@gmail.com>
2023-09-09 02:58:34 +0800

    test run build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/eggsy/" : "/",
+  // base: process.env.NODE_ENV === "production" ? "/eggsy/" : "/",
+  base: '/eggsy/', // Use your repository name as the base path
+  build: {
+    rollupOptions: {
+      input: {
+        main: process.env.NODE_ENV === 'production' ? '/eggsy/index.html' : '/index.html',
+        // Add the route for the custom 404.html page
+        '404': '/404.html',
+      },
+    },
 });
