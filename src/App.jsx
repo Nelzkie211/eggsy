92d6105d3030de59bd400cfbb28f1125a809d268
Nelzkie211 <neilandreireyes211@gmail.com>
2023-09-08 22:40:03 +0800

    add mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import eggsyLogo from './assets/img/eggsy.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import eggsyLogo from "./assets/img/eggsy.png";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
       <div>
-     
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" rel="noreferrer" target="_blank">
           <img src={reactLogo} className="logos logo react" alt="React logo" />
         </a>
       </div>
-      <div>   
-        <a href="https://vitejs.dev" target="_blank">
+      <div>
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
           <img src={eggsyLogo} className="logo eggsy" alt="Vite logo" />
         </a>
       </div>
-     
-      <p className="read-the-docs">
-        Coming soon
-      </p>
-      
+      <p className="read-the-docs">Coming soon</p>
       <p className="read-the-docs">
         <i>@Nelzkie</i>
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
